1a771c149b9b1e8e117c047ebc81be183025dbe2
Elena Caraba <suguleasa@gmail.com>
2013-10-30 17:10:58 -0500

    global variables needed

---
+++ globalVars.py
@@ -0,0 +1,14 @@
+binBnd = [0,110,256];
+AREA_INCLUSION = 16.0; # or a 4x4 small inclusion of pixels
+PROB = 0.1;
+MAX_SIZE = 64;
+MIN_SIZE = 4;
+TOL_LINEARS = 2.0
+TOL_QUADRATICS = 2.0
+TOL_CUBICS = 2.0
+
+class Coordinate(object):
+    def __init__(self,x=-1,y=-1):
+        self.x = x
+        self.y = y
+

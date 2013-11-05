302c6a9bd76ac20ebbd099f121212ccd88052333
Elena Caraba <suguleasa@gmail.com>
2013-11-05 17:25:46 -0600

    introduced alternative minimum size

--- globalVars.py
+++ globalVars.py
@@ -1,8 +1,9 @@
-binBnd = [0,110,256];
-AREA_INCLUSION = 16.0; # or a 4x4 small inclusion of pixels
-PROB = 0.01;
-MAX_SIZE = 64;
-MIN_SIZE = 6;
+binBnd = [0,110,256]
+AREA_INCLUSION = 16.0 # or a 4x4 small inclusion of pixels
+PROB = 0.01
+MAX_SIZE = 64
+MIN_SIZE = 6
+ALT_MIN_SIZE = 3
 TOL_LINEARS = 2.0
 TOL_QUADRATICS = 2.0
 TOL_CUBICS = 2.0

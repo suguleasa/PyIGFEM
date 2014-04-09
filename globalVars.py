1f634b30b3914c25041b2cf6b7bf48037fdb6520
Elena Caraba <suguleasa@gmail.com>
2014-04-09 12:07:21 -0500

    fix

--- globalVars.py
+++ globalVars.py
@@ -1,8 +1,16 @@
-binBnd = [0,110,256]
+#binBnd = [0,180,256] # for real slice
+binBnd = [0,110,256] # for fake images
 AREA_INCLUSION = 16.0 # or a 4x4 small inclusion of pixels
 PROB = 0.01
-MAX_SIZE = 128
-MIN_SIZE = 64#6
+
+#real slice
+#MAX_SIZE = 85
+#MIN_SIZE = 20#6
+
+# fake data
+MAX_SIZE = 256
+MIN_SIZE = 128
+
 ALT_MIN_SIZE = 16#3
 TOL_LINEARS = 2.0
 TOL_QUADRATICS = -2.0 # cancel the cubic approximation by making a negative number
